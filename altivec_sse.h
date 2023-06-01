6705e635da00c7a1d1e166a1517c72c7624a71dd
charlesgregory <charles.gregory940@topper.wku.edu>
2023-05-31 22:42:32 -0400

    fix macros

--- altivec_sse.h
+++ altivec_sse.h
@@ -14,10 +14,10 @@ static inline __m128i _mm_max_epu8(__m128i a, __m128i b) { return vec_max(a, b);
 static inline __m128i _mm_set1_epi8(int8_t n) { return (__m128i)vec_splats(n); }
 static inline __m128i _mm_subs_epu8(__m128i a, __m128i b) { return vec_subs(a, b); }
 
-#define M128I(a)  (vector signed short)((a))
-#define UM128I(a) (vector unsigned short)((a))
-#define S16(a)    (vector unsigned char)((a))
-#define U16(a)    (vector unsigned char)((a))
+#define M128I(a)  (vector unsigned char)((a))
+#define UM128I(a) (vector unsigned char)((a))
+#define S16(a)    (vector signed short)((a))
+#define U16(a)    (vector unsigned short)((a))
 
 static inline __m128i _mm_adds_epi16(__m128i a, __m128i b) { return M128I(vec_adds(S16(a), S16(b))); }
 static inline __m128i _mm_cmpgt_epi16(__m128i a, __m128i b) { return UM128I(vec_cmpgt(S16(a), S16(b))); }
